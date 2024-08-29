d49236744bf7dc89dfd1b316bbe22888f5b9738b
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-08-30 01:52:52 +0800

    open TF32

--- scripts/train/dpo_2_flash-attn.sh
+++ scripts/train/dpo_2_flash-attn.sh
@@ -81,7 +81,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --warmup_ratio 0.1 \
     --lr_scheduler_type "linear" \
     --logging_steps 1 \
-    --tf32 False \
+    --tf32 True \
     --model_max_length 4096 \
     --gradient_checkpointing True \
     --dataloader_num_workers 16 \
