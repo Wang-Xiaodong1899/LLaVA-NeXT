cb3e2cf7faa5efd209e4afd1fd998df63f06e045
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-08-31 01:17:17 +0800

    eval on 4A100

--- scripts/video/eval/correctness.sh
+++ scripts/video/eval/correctness.sh
@@ -4,14 +4,14 @@ CKPT=$1
 SAVE_NAME=$2
 FRAMES=$3
 
-# bash scripts/video/eval/video_generic.sh $CKPT vicuna_v1 $FRAMES 2 average no_token True $SAVE_NAME
+bash scripts/video/eval/video_generic.sh $CKPT vicuna_v1 $FRAMES 2 average no_token True $SAVE_NAME
 
-# python llava/eval/evaluate/evaluate_benchmark_1_correctness.py \
-#     --pred_path results/answer-video-generic-${SAVE_NAME}.jsonl \
-#     --output_dir results/correctness_${SAVE_NAME} \
-#     --output_json results/review-video-correctness-${SAVE_NAME}.jsonl \
-#     --api_key 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 \
-#     --num_tasks 1
+python llava/eval/evaluate/evaluate_benchmark_1_correctness.py \
+    --pred_path results/answer-video-generic-${SAVE_NAME}.jsonl \
+    --output_dir results/correctness_${SAVE_NAME} \
+    --output_json results/review-video-correctness-${SAVE_NAME}.jsonl \
+    --api_key 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 \
+    --num_tasks 1
 
 # # orientation
 # python llava/eval/evaluate/evaluate_benchmark_2_detailed_orientation.py \
@@ -22,10 +22,10 @@ FRAMES=$3
 #     --num_tasks 1
 
 # context
-python llava/eval/evaluate/evaluate_benchmark_3_context.py \
-    --pred_path results/answer-video-generic-${SAVE_NAME}.jsonl \
-    --output_dir results/context_${SAVE_NAME} \
-    --output_json results/review-video-context-${SAVE_NAME}.jsonl \
-    --api_key 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 \
-    --num_tasks 1
+# python llava/eval/evaluate/evaluate_benchmark_3_context.py \
+#     --pred_path results/answer-video-generic-${SAVE_NAME}.jsonl \
+#     --output_dir results/context_${SAVE_NAME} \
+#     --output_json results/review-video-context-${SAVE_NAME}.jsonl \
+#     --api_key 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 \
+#     --num_tasks 1
 
---
+++ scripts/video/eval/total.sh
@@ -0,0 +1,5 @@
+bash scripts/video/eval/consistency.sh /volsparse2/wxd/projects/LLaVA-NeXT/llava-next/llava_dpo_17k_flash_attn/checkpoint-1000/ dpo_17k_1000step 16
+
+bash scripts/video/eval/temporal.sh /volsparse2/wxd/projects/LLaVA-NeXT/llava-next/llava_dpo_17k_flash_attn/checkpoint-1000/ dpo_17k_1000step 16
+
+bash scripts/video/eval/correctness.sh /volsparse2/wxd/projects/LLaVA-NeXT/llava-next/llava_dpo_17k_flash_attn/checkpoint-1000/ dpo_17k_1000step 16
