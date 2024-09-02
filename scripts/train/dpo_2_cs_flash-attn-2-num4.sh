da209a8f5e21db9131398f1293b5ca1b2270c92a
Wang-Xiaodong1899 <1443472206@qq.com>
2024-09-02 20:36:39 +0800

    adapt path for 8V100

--- scripts/train/dpo_2_cs_flash-attn-2-num4.sh
+++ scripts/train/dpo_2_cs_flash-attn-2-num4.sh
@@ -20,7 +20,7 @@ export CUDA_VISIBLE_DEVICES=$gpu_ids
 n_gpu=$(echo $gpu_ids | tr "," "\n" | wc -l)
 echo "Using $n_gpu GPUs: $gpu_ids"
 
-output_dir=/root/autodl-fs/ckpt/${WANDB_PROJECT}/${WANDB_NAME}
+output_dir=/root/autodl-tmp/ckpt/${WANDB_PROJECT}/${WANDB_NAME}
 mkdir -p $output_dir
 
 # DATA
