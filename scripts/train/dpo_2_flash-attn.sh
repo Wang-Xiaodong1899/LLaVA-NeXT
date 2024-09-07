f5ac5ab2fc428ba9e67ea72ca47c4674ebadc1a7
Wang-Xiaodong1899 <1443472206@qq.com>
2024-09-07 14:24:00 +0800

    bs=4, no accumu

--- scripts/train/dpo_2_flash-attn.sh
+++ scripts/train/dpo_2_flash-attn.sh
@@ -71,7 +71,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --num_train_epochs 3 \
     --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 1500 \
