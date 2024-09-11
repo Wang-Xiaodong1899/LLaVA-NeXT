4fdd60053f089aaf59726d4d697834821de8278a
Wang-Xiaodong1899 <1443472206@qq.com>
2024-09-11 22:03:54 +0800

    try different weight

--- scripts/train/dpo_2_cf_flash-attn-fast-reg-no-rej-no-upsample-add-base.sh
+++ scripts/train/dpo_2_cf_flash-attn-fast-reg-no-rej-no-upsample-add-base.sh
@@ -12,7 +12,7 @@ ROOT=$2
 
 # export WANDB_MODE=disabled
 export WANDB_PROJECT=llava-next-jf-4A100
-export WANDB_NAME=llava_dpo_17k_nll-loss-chosen-fast-reg-no-rej-no-upsample-add-base1
+export WANDB_NAME=llava_dpo_17k_nll-loss-chosen-fast2-reg-no-rej1-no-upsample-add-base2
 
 # gpu_ids=0
 gpu_ids=0,1,2,3
@@ -50,10 +50,10 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --enable_video_fast_num 8 \
     --ignore_rejected True \
     --enable_video_fast_upsampler False \
-    --dpo_alpha 1.0 --beta 0.1 --gamma 0 \
+    --dpo_alpha 2.0 --beta 0.1 --gamma 0 \
     --nll_alpha 1.0 \
     --cond_alpha 1.0 \
-    --base_alpha 1.0 \
+    --base_alpha 2.0 \
     --data_path=$data_path \
     --image_folder xxx \
     --video_folder ${ROOT}/data/shareVideoGPTV/dpo_train_data \
