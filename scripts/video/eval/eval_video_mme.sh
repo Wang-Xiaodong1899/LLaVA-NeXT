f85158f3599b8a27d2025227744cc7294117a63a
Wang-Xiaodong1899 <1443472206@qq.com>
2024-09-21 16:28:44 +0800

    add overerite in eval_video_mme

--- scripts/video/eval/eval_video_mme.sh
+++ scripts/video/eval/eval_video_mme.sh
@@ -4,10 +4,12 @@ CKPT=$1
 SAVE_NAME=$2
 FRAMES=$3
 DURATION=$4
+OVERWRITE=${5:False}
 
 #eval_frame: 16 (align with finetuning)
-
-bash scripts/video/eval/video_mme.sh $CKPT vicuna_v1 $FRAMES 2 average no_token True $SAVE_NAME $DURATION
+if [ "$OVERWRITE" = True ]; then
+    bash scripts/video/eval/video_mme.sh $CKPT vicuna_v1 $FRAMES 2 average no_token True $SAVE_NAME $DURATION
+fi
 
 python playground/demo/eval_video_mme.py \
     --results_file results/answer-video-mme-${SAVE_NAME}.json  \
