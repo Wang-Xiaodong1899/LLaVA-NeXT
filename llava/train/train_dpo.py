7786df4e307c6202f897babd1158afb04c9f35c7
Wang-Xiaodong1899 <1443472206@qq.com>
2024-09-24 01:18:04 +0800

    or "llava" in model_args.model_name_or_path.lower()

--- llava/train/train_dpo.py
+++ llava/train/train_dpo.py
@@ -1432,6 +1432,7 @@ def get_model(model_args, training_args, bnb_model_from_pretrained_args):
     elif (
         "wizardlm-2" in model_args.model_name_or_path.lower()
         or "vicuna" in model_args.model_name_or_path.lower()
+        or "llava" in model_args.model_name_or_path.lower()
         or "llama" in model_args.model_name_or_path.lower()
         or "yi" in model_args.model_name_or_path.lower()
         or "nous-hermes" in model_args.model_name_or_path.lower()
@@ -1511,6 +1512,7 @@ def get_model(model_args, training_args, bnb_model_from_pretrained_args):
         elif (
             "wizardlm-2" in model_args.model_name_or_path.lower()
             or "vicuna" in model_args.model_name_or_path.lower()
+            or "llava" in model_args.model_name_or_path.lower()
             or "llama" in model_args.model_name_or_path.lower()
             or "yi" in model_args.model_name_or_path.lower()
             or "nous-hermes" in model_args.model_name_or_path.lower()
