c74b8fc7f7df1059e52809680ceb74cdcf6c7e6a
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-09-25 01:13:55 +0800

    minor

--- llava/train/llava_trainer.py
+++ llava/train/llava_trainer.py
@@ -500,7 +500,7 @@ class LLaVADPOTrainer(DPOTrainer):
     def _get_train_sampler(self) -> Optional[torch.utils.data.Sampler]:
         if self.train_dataset is None or not has_length(self.train_dataset):
             return None
-        import pdb; pdb.set_trace()
+        # import pdb; pdb.set_trace()
         if self.args.group_by_modality_length:
             lengths = self.train_dataset.modality_lengths
             return LengthGroupedSampler(
