c9a6da7269068054ffb24f5492ba7d5b8ecc911a
Wang-Xiaodong1899 <1443472206@qq.com>
2024-09-26 15:05:47 +0800

    add download llava_hound

---
+++ scripts/process/llava_hound_300k.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+base_url="https://huggingface.co/datasets/ShareGPTVideo/train_video_and_instruction/resolve/main/train_300k/chunk_"
+
+
+start=0
+end=15
+
+
+for i in $(seq $start $end)
+do
+    file_name="${i}.tar.gz"
+    url="${base_url}${i}.zip"
+
+    echo "Downloading ${file_name}..."
+
+    wget --continue --retry-connrefused --waitretry=5 --tries=5 --timeout=30 "${url}"
+
+
+    if [ $? -eq 0 ]; then
+        echo "${file_name} downloaded successfully."
+    else
+        echo "Failed to download ${file_name}. Retrying..."
+
+        wget --continue --retry-connrefused --waitretry=5 --tries=5 --timeout=30 "${url}"
+        if [ $? -eq 0 ]; then
+            echo "${file_name} downloaded successfully after retry."
+        else
+            echo "Failed to download ${file_name} after multiple attempts."
+        fi
+    fi
+done
+
+echo "Download completed."
