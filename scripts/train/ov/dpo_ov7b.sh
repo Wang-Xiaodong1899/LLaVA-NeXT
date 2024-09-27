7cd50f37a40e830e1471761dbdb10e87f03d564e
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-09-28 01:57:02 +0800

    minor fix

--- scripts/train/ov/dpo_ov7b.sh
+++ scripts/train/ov/dpo_ov7b.sh
@@ -61,7 +61,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --image_aspect_ratio anyres_max_9 \
     --image_grid_pinpoints "(1x1),...,(6x6)" \
     --mm_patch_merge_type spatial_unpad \
-    --bf16 True \
+    --fp16 True \
     --run_name $WANDB_NAME \
     --output_dir $output_dir \
     --num_train_epochs 3 \
@@ -77,7 +77,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --warmup_ratio 0.1 \
     --lr_scheduler_type "cosine" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 4096 \
     --gradient_checkpointing True \
     --dataloader_num_workers 16 \
