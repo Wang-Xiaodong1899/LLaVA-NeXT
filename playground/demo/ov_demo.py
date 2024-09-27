b67cf6b3b09a6fd5ba5693dbdfe6bdd932d051b2
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-09-28 02:46:27 +0800

    debug

--- playground/demo/ov_demo.py
+++ playground/demo/ov_demo.py
@@ -49,6 +49,8 @@ image_tensors = []
 frames = image_processor.preprocess(video_frames, return_tensors="pt")["pixel_values"].half().cuda()
 image_tensors.append(frames)
 
+import pdb; pdb.set_trace()
+
 # Prepare conversation input
 conv_template = "qwen_1_5"
 question = f"{DEFAULT_IMAGE_TOKEN}\nDescribe what's happening in this video."
