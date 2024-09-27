0b36cc2756590501c8d74a57d7954078f40b32d4
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-09-28 01:30:24 +0800

    add --frames_upbound 16

--- scripts/train/ov/dpo_ov7b.sh
+++ scripts/train/ov/dpo_ov7b.sh
@@ -49,6 +49,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --image_folder xxx \
     --video_folder ${ROOT}/data/shareVideoGPTV/dpo_train_data \
     --freeze_mm_mlp_adapter True \
+    --frames_upbound 16 \
     --vision_tower ${VISION_MODEL_VERSION} \
     --mm_projector_type mlp2x_gelu \
     --mm_vision_select_layer -2 \
