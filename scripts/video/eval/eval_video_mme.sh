67f773ba20b0762d5ce53f1464a1e647bb26b984
Wang-Xiaodong1899 <1443472206@qq.com>
2024-09-28 11:24:08 +0800

    eval ov Video-MME

--- scripts/video/eval/eval_video_mme.sh
+++ scripts/video/eval/eval_video_mme.sh
@@ -4,14 +4,27 @@ CKPT=$1
 SAVE_NAME=$2
 FRAMES=$3
 DURATION=$4 # short
-OVERWRITE=${5:False}
+OVERWRITE=${5:False} # overwrite previous eval result
+
+CONV_MODE=$6
+POOL_STRIDE=$7
+POOL_MODE=$8
+NEWLINE_POSITION=$9
 
 #eval_frame: 16 (align with finetuning)
 if [ "$OVERWRITE" = True ]; then
-    bash scripts/video/eval/video_mme.sh $CKPT vicuna_v1 $FRAMES 2 average no_token True $SAVE_NAME $DURATION
+    bash scripts/video/eval/video_mme.sh $CKPT $CONV_MODE $FRAMES $POOL_STRIDE $POOL_MODE $NEWLINE_POSITION True $SAVE_NAME $DURATION
 fi
 
 python playground/demo/eval_video_mme.py \
     --results_file results/answer-video-mme-${SAVE_NAME}.json  \
     --video_duration_type $DURATION \
     --return_categories_accuracy
+
+# tip
+
+# vicuna: llava-next-video
+# export DECORD_EOF_RETRY_MAX=20480 && CUDA_VISIBLE_DEVICES=1 bash scripts/video/eval/eval_video_mme.sh ckpt_path name-medium 16 medium True vicuna_v1 2 average no_token
+
+# qwen: onevision
+# export DECORD_EOF_RETRY_MAX=20480 && CUDA_VISIBLE_DEVICES=1 bash scripts/video/eval/eval_video_mme.sh /workspace/wxd/LLaVA-NeXT/qwen/llava-onevision-qwen2-7b-ov 7b-ov-short 16 short True qwen_1_5 1 bilinear one_token
