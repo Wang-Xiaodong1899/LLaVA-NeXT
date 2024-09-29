dd52973240f10f1b3208e76b6bfcd39217558981
Wang-Xiaodong1899 <1443472206@qq.com>
2024-09-29 16:32:17 +0800

    minor fix

--- scripts/train/ov/dpo_ov7b.sh
+++ scripts/train/ov/dpo_ov7b.sh
@@ -65,9 +65,9 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --run_name $WANDB_NAME \
     --output_dir $output_dir \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 2 \
+    --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 2 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 500 \
