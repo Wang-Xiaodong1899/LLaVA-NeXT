1d172ef955f6c008241bdcc71ded093f770f49a9
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-01 14:11:51 +0800

    fix bug

--- scripts/process/gen_prefer_aug-f4.sh
+++ scripts/process/gen_prefer_aug-f4.sh
@@ -35,8 +35,8 @@ fi
 python scripts/process/self_generate_preference.py \
     --model-path $CKPT \
     --video_root ${VIDEO_PATH} \
-    --add-aug True \
-    --skip-chosen False \
+    --add-aug true \
+    --skip-chosen false \
     --output_dir /volsparse1/wxd/data/self-gen/video_aug/$SAVE_DIR \
     --output_name ov-7b-aug_f4_${START}_${END} \
     --jsonl-file $JSONLFILE \
