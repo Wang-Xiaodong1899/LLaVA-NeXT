a7f7f6a20d37df851e1c8957c3112e7b4b7eb1ed
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-10-07 01:30:17 +0800

    polish prompt

--- playground/demo/ov_demo_nusc.py
+++ playground/demo/ov_demo_nusc.py
@@ -81,8 +81,9 @@ This is a video of a car driving from a front-view camera. Please answer the fol
 Weather: (e.g., sunny, cloudy, rainy, etc.)
 Time: (e.g., daytime, nighttime, etc.)
 Road environment:
+Critical objects:
 Driving action: Select one of [Speed up, Slow down, Speed up rapidly, Slow down rapidly, Go straight slowly, Go straight at a constant speed, Turn left, Turn right, Change lane to the left, Change lane to the right, Shift slightly to the left, Shift slightly to the right, Stop, Wait], or select multiple action sequences, up to a maximum of 4 action sequences.
-Concise scene summary:
+Scene summary:
 """
 # question = f"{DEFAULT_IMAGE_TOKEN}\nDescribe what's happening in this video."
 print(question)
