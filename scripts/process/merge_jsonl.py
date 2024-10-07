3e5f86361c7ef3f7726450d5248f3c7716497490
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-07 18:33:28 +0800

    minor

--- scripts/process/merge_jsonl.py
+++ scripts/process/merge_jsonl.py
@@ -3,12 +3,15 @@ import json
 
 # Define the input folder path and output file path
 input_folder = '/volsparse1/wxd/data/self-gen/video_ov-72b/llava-onevision-qwen2-72b-ov-sft_qwen_1_5_frames_32_stride_1'
-output_file = 'ov-72b-f32_0_8000.jsonl'
+output_file = 'ov-72b-f32_0_16000.jsonl'
 
 # Create the output file
 with open(output_file, 'w', encoding='utf-8') as outfile:
     # Iterate through each file in the folder
-    for filename in os.listdir(input_folder):
+    filenames = list(os.listdir(input_folder))
+    filenames.sort()
+    print(filenames)
+    for filename in filenames:
         if filename.endswith('.jsonl') and filename.startswith('ov'):
             with open(os.path.join(input_folder, filename), 'r', encoding='utf-8') as infile:
                 for line in infile:
