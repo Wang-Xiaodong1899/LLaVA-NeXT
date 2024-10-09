61a10643c2baa940606a46282d51bc151d1fba3b
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-10-09 11:27:19 +0800

    minor

--- scripts/train/ov/dpo_ov7b_gen_sft-align.sh
+++ scripts/train/ov/dpo_ov7b_gen_sft-align.sh
@@ -70,7 +70,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 200 \
+    --save_steps 100 \
     --save_total_limit 2 \
     --learning_rate $lr \
     --weight_decay 0. \
