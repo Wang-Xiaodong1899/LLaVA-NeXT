6d4213335454d3e88fa5c4fa4b248670306bf60e
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-13 17:21:01 +0800

    minor merge

--- scripts/process/merge_jsonl.py
+++ scripts/process/merge_jsonl.py
@@ -2,8 +2,8 @@ import os
 import json
 
 # Define the input folder path and output file path
-input_folder = '/volsparse1/wxd/data/self-gen/video_ov-72b/llava-onevision-qwen2-72b-ov-sft_qwen_1_5_frames_32_stride_1'
-output_file = 'ov-72b-f32_0_16000.jsonl'
+input_folder = r'C:\Users\wangxiaodong\Desktop\checkpoint-500_vicuna_v1_frames_4_stride_2'
+output_file = 'next-dpo-7b-iter2-f4_8000_16000.jsonl'
 
 # Create the output file
 with open(output_file, 'w', encoding='utf-8') as outfile:
@@ -12,7 +12,7 @@ with open(output_file, 'w', encoding='utf-8') as outfile:
     filenames.sort()
     print(filenames)
     for filename in filenames:
-        if filename.endswith('.jsonl') and filename.startswith('ov'):
+        if filename.endswith('.jsonl') and filename.startswith('next'):
             with open(os.path.join(input_folder, filename), 'r', encoding='utf-8') as infile:
                 for line in infile:
                     outfile.write(line)
