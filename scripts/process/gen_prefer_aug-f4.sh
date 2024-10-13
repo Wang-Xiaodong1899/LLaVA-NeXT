8f2286ea90eb15a776ab975c688f081a8504aee5
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-13 22:32:50 +0800

    iter continue

--- scripts/process/gen_prefer_aug-f4.sh
+++ scripts/process/gen_prefer_aug-f4.sh
@@ -37,7 +37,7 @@ python scripts/process/self_generate_preference.py \
     --video_root ${VIDEO_PATH} \
     --add-aug true \
     --output_dir /volsparse1/wxd/data/self-gen/video_aug/$SAVE_DIR \
-    --output_name ov-7b-aug_f4_${START}_${END} \
+    --output_name next-dpo-7b-aug_f4_${START}_${END} \
     --jsonl-file $JSONLFILE \
     --start $START \
     --end $END \
@@ -60,3 +60,6 @@ python scripts/process/self_generate_preference.py \
 
 # iter-2
 # CUDA_VISIBLE_DEVICES=0 bash scripts/process/gen_prefer_aug-f4.sh /volsparse1/wxd/ckpt/llava-next-jf-4A100/Next-DPO-iter2/checkpoint-500/ vicuna_v1 4 2 average no_token True /volsparse1/wxd/data/llava_hound/shareVideoGPTV/QA/ /volsparse1/wxd/data/llava_hound/filtered_video_id.jsonl 8000 10000 224
+
+# iter-3
+# CUDA_VISIBLE_DEVICES=0 bash scripts/process/gen_prefer_aug-f4.sh /volsparse1/wxd/ckpt/llava-next-jf-4A100/llava_dpo_17k_flash-attn_DPO_iter3_8k_data/checkpoint-500/ vicuna_v1 4 2 average no_token True /volsparse1/wxd/data/llava_hound/shareVideoGPTV/QA/ /volsparse1/wxd/data/llava_hound/filtered_video_id.jsonl 16000 18000 224
