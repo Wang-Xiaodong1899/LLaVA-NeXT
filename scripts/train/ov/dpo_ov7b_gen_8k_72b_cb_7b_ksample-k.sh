8187e2911e7bb5983e5204e817fc8b204af4527d
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-15 01:55:59 +0800

    epoch3

--- scripts/train/ov/dpo_ov7b_gen_8k_72b_cb_7b_ksample-k.sh
+++ scripts/train/ov/dpo_ov7b_gen_8k_72b_cb_7b_ksample-k.sh
@@ -64,13 +64,13 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --bf16 True \
     --run_name $WANDB_NAME \
     --output_dir $output_dir \
-    --num_train_epochs 1 \
+    --num_train_epochs 3 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 300 \
+    --save_steps 500 \
     --save_total_limit 2 \
     --learning_rate $lr \
     --weight_decay 0. \
