3141b188e6679a4d873914a65c65ae1f6a3eacb4
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-21 12:03:34 +0800

    debug

--- llava/train/train_dpo_avg.py
+++ llava/train/train_dpo_avg.py
@@ -1311,7 +1311,9 @@ class DPODataCollator(DPODataCollatorWithPadding):
                 #     padded_batch[k] = padded_batch[k].flip(dims=[1])
             else:
                 padded_batch[k] = [ex[k] for ex in batch]
-        import pdb; pdb.set_trace()
+        print(padded_batch.keys())
+        for k, item in padded_batch:
+            print(f"{k}: {len(item)}")
         for k in ["chosen_input_ids", "rejected_input_ids", "answer_input_ids"]:
             attn_k = k.replace("input_ids", "attention_mask")
             padded_batch[attn_k] = padded_batch[k].ne(self.tokenizer.pad_token_id)
