624957f7220ea691307d042b6ec46f00fc28f639
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-25 11:12:08 +0800

    minor fix

--- scripts/train/next/dpo_2_flash-attn_self-iter2.sh
+++ scripts/train/next/dpo_2_flash-attn_self-iter2.sh
@@ -46,7 +46,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --deepspeed scripts/zero2.json \
     --model_name_or_path /volsparse1/wxd/ckpt/llava-next-jf-4A100/llava-next-vicuna-dpo-iter1/checkpoint-3000 \
     --version $PROMPT_VERSION \
-    --dpo_alpha 1.0 --beta 2.0 --gamma 0 \
+    --dpo_alpha 1.0 --beta 0.1 --gamma 0 \
     --data_path=$data_path \
     --image_folder xxx \
     --video_folder /volsparse1/wxd/data/llava_hound/shareVideoGPTV/QA \
