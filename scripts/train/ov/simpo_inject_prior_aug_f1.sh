46c5557acb956c84bee3f7e99d618b2459728426
Wang-Xiaodong1899 <1443472206@qq.com>
2024-10-29 15:08:41 +0800

    back

--- scripts/train/ov/simpo_inject_prior_aug_f1.sh
+++ scripts/train/ov/simpo_inject_prior_aug_f1.sh
@@ -62,13 +62,13 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --image_aspect_ratio anyres_max_9 \
     --image_grid_pinpoints "(1x1),...,(6x6)" \
     --mm_patch_merge_type spatial_unpad \
-    --bf16 True \
+    --fp16 True \
     --run_name $WANDB_NAME \
     --output_dir $output_dir \
     --num_train_epochs 1 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 2 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 200 \
@@ -78,7 +78,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --warmup_ratio 0.1 \
     --lr_scheduler_type "linear" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 3584 \
     --gradient_checkpointing True \
     --dataloader_num_workers 16 \
