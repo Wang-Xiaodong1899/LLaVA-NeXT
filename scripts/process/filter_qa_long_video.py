fd94119142b1faceac3bbfe3aafdd365109dba50
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-03 19:38:35 +0800

    fix

---
+++ scripts/process/filter_qa_long_video.py
@@ -0,0 +1,28 @@
+import json
+import os
+
+target_directory = '/volsparse1/wxd/data/llava_hound/shareVideoGPTV/'
+# Change the current working directory
+os.chdir(target_directory)
+
+# Define the path to the JSONL file and the QA folder
+jsonl_file_path = '/volsparse1/wxd/data/llava_hound/chatgpt_qa_900k.jsonl'  # Replace with your JSONL file path
+
+matched_data = []
+with open('/volsparse1/wxd/data/llava_hound/shareVideoGPTV/long_video_ge_64_qa.jsonl', 'r') as f:
+    long_id = json.load(f)
+
+# Read the JSONL file
+with open(jsonl_file_path, 'r') as jsonl_file:
+    for line in jsonl_file:
+        entry = json.loads(line)  # Parse the JSON line into a dictionary
+        if entry['video'] in long_id:  # Check if the video exists in the QA folder
+            matched_data.append(entry)  # Add matching entry to the list
+
+# Write the matching entries to a new JSONL file
+with open('/volsparse1/wxd/data/llava_hound/shareVideoGPTV/filtered_long_video_id_1103.jsonl', 'w') as output_file:
+    for item in matched_data:
+        json.dump(item, output_file)  # Write the JSON entry to the file
+        output_file.write('\n')  # Add a newline after each entry
+
+print(f"Kept {len(matched_data)} entries.")  # Print the count of kept entries
