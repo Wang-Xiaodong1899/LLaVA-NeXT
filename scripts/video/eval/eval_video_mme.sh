a3b35a9ad2a58a1750563028c51374d497644445
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-08 13:39:30 +0800

    python to python3

--- scripts/video/eval/eval_video_mme.sh
+++ scripts/video/eval/eval_video_mme.sh
@@ -17,7 +17,7 @@ if [ "$OVERWRITE" = True ]; then
     bash scripts/video/eval/video_mme.sh $CKPT $CONV_MODE $FRAMES $POOL_STRIDE $POOL_MODE $NEWLINE_POSITION True $SAVE_NAME $DURATION $RESOLUTION
 fi
 
-python playground/demo/eval_video_mme.py \
+python3 playground/demo/eval_video_mme.py \
     --results_file results/answer-video-mme-${SAVE_NAME}.json  \
     --video_duration_type $DURATION \
     --return_categories_accuracy
@@ -25,7 +25,7 @@ python playground/demo/eval_video_mme.py \
 # tip
 
 # vicuna: llava-next-video
-# export DECORD_EOF_RETRY_MAX=20480 && CUDA_VISIBLE_DEVICES=1 bash scripts/video/eval/eval_video_mme.sh ckpt_path name-medium 16 medium True vicuna_v1 2 average no_token 336
+# export DECORD_EOF_RETRY_MAX=40960 && CUDA_VISIBLE_DEVICES=2 bash scripts/video/eval/eval_video_mme.sh /volsparse2/wxd/ckpt/llava-next-jf-4A100/llava_vicuna_simpo_inject_8k_aug_8k_f32_stride_3/checkpoint-100/ simpo-next-f32-stride-3-medium 32 medium True vicuna_v1 2 average no_token 336
 
 # qwen: onevision
-# export DECORD_EOF_RETRY_MAX=20480 && CUDA_VISIBLE_DEVICES=1 bash scripts/video/eval/eval_video_mme.sh /workspace/wxd/LLaVA-NeXT/qwen/llava-onevision-qwen2-7b-ov 7b-ov-short 16 short True qwen_1_5 1 bilinear one_token 384
+# export DECORD_EOF_RETRY_MAX=40960 && CUDA_VISIBLE_DEVICES=0 bash scripts/video/eval/eval_video_mme.sh /volsparse2/wxd/ckpt/llava-ov-jf-4A100/llava_qwen_simpo_inject_prior_aug-f2-8k-stride_3/checkpoint-400/ 7b-ov-stride3-short 32 short True qwen_1_5 1 bilinear one_token 384
