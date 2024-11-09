4715f143933dbdd8227c2826285d024b0dec2db9
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-11-10 02:41:42 +0800

    add stride for video-chatgpt

--- scripts/video/eval/temporal.sh
+++ scripts/video/eval/temporal.sh
@@ -3,10 +3,11 @@
 CKPT=$1
 SAVE_NAME=$2
 FRAMES=$3
+STRIDE=$4
 
 #eval_frame: 16 (align with finetuning)
 
-# bash scripts/video/eval/video_temporal.sh $CKPT vicuna_v1 $FRAMES 2 average no_token True $SAVE_NAME
+bash scripts/video/eval/video_temporal.sh $CKPT vicuna_v1 $FRAMES $STRIDE average no_token True $SAVE_NAME
 
 python llava/eval/evaluate/evaluate_benchmark_4_temporal.py \
     --pred_path results/answer-video-temporal-${SAVE_NAME}.jsonl \
