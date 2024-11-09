1a3787133022277469774b88a388e1151dbcd61f
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-11-10 03:08:53 +0800

    minor

--- scripts/video/eval/correctness.sh
+++ scripts/video/eval/correctness.sh
@@ -7,12 +7,12 @@ STRIDE=${4:-2}
 
 bash scripts/video/eval/video_generic.sh $CKPT vicuna_v1 $FRAMES $STRIDE average no_token True $SAVE_NAME
 
-# python3 llava/eval/evaluate/evaluate_benchmark_1_correctness.py \
-#     --pred_path results/answer-video-generic-${SAVE_NAME}.jsonl \
-#     --output_dir results/correctness_${SAVE_NAME}_0613 \
-#     --output_json results/review-video-correctness-${SAVE_NAME}_0613.jsonl \
-#     --api_key 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 \
-#     --num_tasks 1
+python3 llava/eval/evaluate/evaluate_benchmark_1_correctness.py \
+    --pred_path results/answer-video-generic-${SAVE_NAME}.jsonl \
+    --output_dir results/correctness_${SAVE_NAME}_0613 \
+    --output_json results/review-video-correctness-${SAVE_NAME}_0613.jsonl \
+    --api_key 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 \
+    --num_tasks 1
 
 # # orientation
 python3 llava/eval/evaluate/evaluate_benchmark_2_detailed_orientation.py \
