889c5b7a707ab59c63fbac6f7adccd768b003b97
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-09 21:50:05 +0800

    try save 50 steps

--- scripts/train/next/simpo_inject_stride3_iter2.sh
+++ scripts/train/next/simpo_inject_stride3_iter2.sh
@@ -12,7 +12,7 @@ ROOT=$2
 
 # export WANDB_MODE=disabled
 export WANDB_PROJECT=llava-next-jf-4A100
-export WANDB_NAME=llava_vicuna_simpo_inject_8k_aug_8k_f32_stride_3_iter2_4k
+export WANDB_NAME=llava_vicuna_simpo_inject_8k_aug_8k_f32_stride_3_iter2_4k_copy
 
 gpu_ids=0,1,2,3
 export CUDA_VISIBLE_DEVICES=$gpu_ids
@@ -74,7 +74,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --gradient_accumulation_steps 2 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 100 \
+    --save_steps 50 \
     --save_total_limit 3 \
     --learning_rate $lr \
     --weight_decay 0. \
