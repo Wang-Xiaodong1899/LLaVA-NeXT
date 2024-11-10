0b1185f3e8723f31eb0911574ed633375e8db170
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-11-10 14:55:39 +0800

    debug

--- llava/eval/evaluate/evaluate_benchmark_4_temporal.py
+++ llava/eval/evaluate/evaluate_benchmark_4_temporal.py
@@ -196,6 +196,7 @@ def main():
     for file_name in os.listdir(output_dir):
         if file_name.endswith(".json"):
             file_path = os.path.join(output_dir, file_name)
+            print(file_path)
             with open(file_path, "r", encoding='utf-8', errors='ignore') as json_file:
                 content = json.load(json_file)
                 combined_contents[file_name[:-5]] = content
