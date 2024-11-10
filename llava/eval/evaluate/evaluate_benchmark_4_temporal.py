9c0403307341be6dbeb02c28a34ec61433b25f54
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-11-10 11:32:53 +0800

    debug

--- llava/eval/evaluate/evaluate_benchmark_4_temporal.py
+++ llava/eval/evaluate/evaluate_benchmark_4_temporal.py
@@ -88,7 +88,7 @@ def annotate(prediction_set, caption_files, output_dir):
                 ]
             )
             # print(completion.model)
-            print(completion)
+            # print(completion)
             # Convert response to a Python dictionary.
             response_message = completion.choices[0].message.content
             response_dict = ast.literal_eval(response_message)
