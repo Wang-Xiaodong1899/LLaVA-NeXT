32cfbcfc900b0420541ee9a6559adcc3d8a08ae8
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-12 10:08:15 +0800

    minor path

--- scripts/train/ov/simpo_inject_prior_aug_f2_caption.sh
+++ scripts/train/ov/simpo_inject_prior_aug_f2_caption.sh
@@ -47,7 +47,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --dpo_alpha 1.0 --beta 2.0 --gamma 0 \
     --data_path=$data_path \
     --image_folder xxx \
-    --video_folder /data/llava_hound/caption/data/ \
+    --video_folder /data/llava_hound/caption/shareVideoGPTV/data/ \
     --freeze_mm_mlp_adapter True \
     --frames_upbound 16 \
     --vision_tower ${VISION_MODEL_VERSION} \
