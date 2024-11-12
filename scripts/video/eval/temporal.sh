a141e484aa0d17b5cc0833f17038174e60c2ec5c
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-12 18:26:13 +0800

    eval 50

--- scripts/video/eval/temporal.sh
+++ scripts/video/eval/temporal.sh
@@ -11,7 +11,7 @@ STRIDE=${4:-2}
 # bash scripts/video/eval/video_temporal.sh $CKPT vicuna_v1 $FRAMES $STRIDE average no_token True $SAVE_NAME
 
 # ov
-# bash scripts/video/eval/video_temporal.sh $CKPT qwen_1_5 $FRAMES $STRIDE bilinear one_token True $SAVE_NAME
+bash scripts/video/eval/video_temporal.sh $CKPT qwen_1_5 $FRAMES $STRIDE bilinear one_token True $SAVE_NAME
 
 python3 llava/eval/evaluate/evaluate_benchmark_4_temporal.py \
     --pred_path results/answer-video-temporal-${SAVE_NAME}.jsonl \
