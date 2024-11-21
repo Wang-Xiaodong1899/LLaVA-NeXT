14774c2318bf4bc40979226bbad8cab80c311ff7
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-21 23:02:35 +0800

    add llava path

--- scripts/image/infer.py
+++ scripts/image/infer.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
 from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path, process_images, tokenizer_image_token
 from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IGNORE_INDEX
