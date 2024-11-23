49f9d3b9b3578fad0716c36ddfd6bb1fbfd9c7f9
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-23 16:14:17 +0800

    fix

--- scripts/train/ov/image_simpo_test.sh
+++ scripts/train/ov/image_simpo_test.sh
@@ -12,7 +12,7 @@ ROOT=$2
 
 # export WANDB_MODE=disabled
 export WANDB_PROJECT=llava-ov-jf-4A100
-export WANDB_NAME=llava_qwen_simpo_inject_debug
+export WANDB_NAME=llava_qwen_simpo_image_debug
 
 # gpu_ids=0
 gpu_ids=2
