4decb2d0b4c8f0ae5e505056ad5e2ede4384e3e8
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-24 14:32:02 +0800

    zero3

--- scripts/train/ov/image_dpo_RLAIFV.sh
+++ scripts/train/ov/image_dpo_RLAIFV.sh
@@ -41,7 +41,7 @@ PROMPT_VERSION="qwen_1_5"
 # ACCELERATE_CPU_AFFINITY=1 torchrun --nproc_per_node="${ARNOLD_WORKER_GPU}" --nnodes="${ARNOLD_WORKER_NUM}" --node_rank="${ARNOLD_ID}" --master_addr="${METIS_WORKER_0_HOST}" --master_port="${port_in_cmd}" \
 torchrun --nproc_per_node=$n_gpu --master_port=$port \
     llava/train/train_dpo.py \
-    --deepspeed scripts/zero2.json \
+    --deepspeed scripts/zero3.json \
     --model_name_or_path /volsparse2/wxd/models/qwen/llava-onevision-qwen2-7b-ov \
     --version $PROMPT_VERSION \
     --dpo_alpha 1.0 --beta 2.0 --gamma 0 \
