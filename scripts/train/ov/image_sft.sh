09dadbfad72f034445867499fd7d5a71a87d9212
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-25 16:11:57 +0800

    minor

--- scripts/train/ov/image_sft.sh
+++ scripts/train/ov/image_sft.sh
@@ -70,7 +70,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 2000 \
+    --save_steps 1000 \
     --save_total_limit 1 \
     --learning_rate 1e-5 \
     --weight_decay 0. \
