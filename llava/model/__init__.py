2e59ad06bfee1ce22b44022f447092922277d37b
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-25 11:55:04 +0800

    fix qwen import

--- llava/model/__init__.py
+++ llava/model/__init__.py
@@ -10,7 +10,7 @@ AVAILABLE_MODELS = {
 }
 
 for model_name, model_classes in AVAILABLE_MODELS.items():
-    if True:
+    try:
         exec(f"from .language_model.{model_name} import {model_classes}")
-    # except Exception as e:
-    #     print(f"Failed to import {model_name} from llava.language_model.{model_name}. Error: {e}")
+    except Exception as e:
+        print(f"Failed to import {model_name} from llava.language_model.{model_name}. Error: {e}")
