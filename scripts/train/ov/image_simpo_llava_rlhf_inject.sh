6f2c5beacb9ff226b4b09d35fd0644408655fc1c
Wang-Xiaodong1899 <1443472206@qq.com>
2024-11-30 20:50:24 +0800

    minor

--- scripts/train/ov/image_simpo_llava_rlhf_inject.sh
+++ scripts/train/ov/image_simpo_llava_rlhf_inject.sh
@@ -24,7 +24,7 @@ output_dir=/volsparse3/wxd/ckpt/${WANDB_PROJECT}/${WANDB_NAME}
 mkdir -p $output_dir
 
 # DATA
-data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo.json
+data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo_ov_inject_all.jsonl
 
 # sudo chmod +x -R .
 # export PYTHONPATH=.
@@ -44,7 +44,8 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --deepspeed scripts/zero3.json \
     --model_name_or_path /volsparse3/wxd/models/qwen/llava-onevision-qwen2-7b-ov \
     --version $PROMPT_VERSION \
-    --dpo_alpha 1.0 --beta 0.1 --gamma 0 \
+    --loss_type simpo \
+    --dpo_alpha 1.0 --beta 2.0 --gamma 0 \
     --data_path=$data_path \
     --image_folder /data/mscoco/train2014 \
     --video_folder xxx \
@@ -72,7 +73,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 250 \
-    --save_total_limit 2 \
+    --save_total_limit 4 \
     --learning_rate $lr \
     --weight_decay 0. \
     --warmup_ratio 0.1 \
