25d87b26fd46863bf927389a5d216330268d69f3
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-03 17:33:46 +0800

    minor

--- scripts/train/ov/image_simpo_llava_rlhf_inject_debug.sh
+++ scripts/train/ov/image_simpo_llava_rlhf_inject_debug.sh
@@ -15,7 +15,7 @@ export WANDB_PROJECT=llava-ov-image-jf-4A100
 export WANDB_NAME=llava_qwen_simpo_llava-rlhf-debate-chosen_aug-rejected-ptest
 
 # gpu_ids=0
-gpu_ids=0,1
+gpu_ids=2
 export CUDA_VISIBLE_DEVICES=$gpu_ids
 n_gpu=$(echo $gpu_ids | tr "," "\n" | wc -l)
 echo "Using $n_gpu GPUs: $gpu_ids"
@@ -26,7 +26,7 @@ mkdir -p $output_dir
 # DATA
 # data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo_ov_inject_all.jsonl
 # data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo_ov_multi-turn_all.jsonl
-data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo_ov_debate-chosen_aug-s224-rejected-sample0.9.jsonl
+data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo_ov_debate-chosen_aug-s224-rejected-sample0.8.jsonl
 
 # sudo chmod +x -R .
 # export PYTHONPATH=.
