4c5f670d447852d388b762460faf343dc645f294
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-09 21:45:16 +0800

    train SI

--- scripts/train/ov/image_simpo_llava_rlhf_inject_SI.sh
+++ scripts/train/ov/image_simpo_llava_rlhf_inject_SI.sh
@@ -26,7 +26,7 @@ mkdir -p $output_dir
 # DATA
 # data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo_ov_inject_all_fix.jsonl
 # data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo_ov_multi-turn_all.jsonl
-data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo_ov_debate-chosen_aug-s224-rejected-sample0.6.jsonl
+data_path=/volsparse3/wxd/data/llava-onevision-data/llava_rlhf_for_dpo_si_debate-chosen_aug-s224-rejected-sample0.6.jsonl
 
 # sudo chmod +x -R .
 # export PYTHONPATH=.
