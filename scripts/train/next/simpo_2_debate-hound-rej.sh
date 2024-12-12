7c303cc030de7d5050f9cce502f6cde8f0da361e
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-12-12 10:16:01 +0800

    minor

--- scripts/train/next/simpo_2_debate-hound-rej.sh
+++ scripts/train/next/simpo_2_debate-hound-rej.sh
@@ -12,7 +12,7 @@ ROOT=$2
 
 # export WANDB_MODE=disabled
 export WANDB_PROJECT=llava-next-jf-4A100
-export WANDB_NAME=llava_simpo_8k_debate-hound-rej
+export WANDB_NAME=llava_simpo_8k_debate-hound-rej-1
 
 # gpu_ids=0
 gpu_ids=0,1,2,3
@@ -74,7 +74,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 500 \
+    --save_steps 200 \
     --save_total_limit 4 \
     --learning_rate $lr \
     --weight_decay 0. \
