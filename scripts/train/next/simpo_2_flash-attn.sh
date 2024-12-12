1434a8fbd24d7413a67800f1da140dd62cb4e9a7
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-12 17:02:40 +0800

    [Important] fix simpo llava-hound

--- scripts/train/next/simpo_2_flash-attn.sh
+++ scripts/train/next/simpo_2_flash-attn.sh
@@ -46,6 +46,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --deepspeed scripts/zero2.json \
     --model_name_or_path /volsparse3/wxd/models/vicuna/LLaVA-NeXT-Video-7B \
     --version $PROMPT_VERSION \
+    --loss_type simpo \
     --dpo_alpha 1.0 --beta 2.0 --gamma 0 \
     --data_path=$data_path \
     --image_folder xxx \
