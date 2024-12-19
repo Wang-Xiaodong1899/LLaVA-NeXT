482ae4abd27b87141b83bff95f4e6afa3c519fb4
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-19 17:23:36 +0800

    sync

--- scripts/video/eval/eval_video_mme.sh
+++ scripts/video/eval/eval_video_mme.sh
@@ -23,7 +23,7 @@ python3 playground/demo/eval_video_mme.py \
     --return_categories_accuracy
 
 # tip
-# export DECORD_EOF_RETRY_MAX=40960 && CUDA_VISIBLE_DEVICES=0 bash scripts/video/eval/eval_video_mme.sh /root/autodl-tmp/ckpt/llava-next-4A800/llava_simpo_8k_debate-hound-ours/checkpoint-500/ simpo-hound-ours-long 16 long True vicuna_v1 2 average no_token 336
+# export DECORD_EOF_RETRY_MAX=40960 && CUDA_VISIBLE_DEVICES=0 bash scripts/video/eval/eval_video_mme.sh /root/autodl-tmp/ckpt/llava-next-4A800/llava_simpo_8k_debate-hound-ours/checkpoint-500/ simpo-hound-ours-short 16 short True vicuna_v1 2 average no_token 336
 
 # vicuna: llava-next-video
 # export DECORD_EOF_RETRY_MAX=40960 && CUDA_VISIBLE_DEVICES=0 bash scripts/video/eval/eval_video_mme.sh /volsparse2/wxd/ckpt/llava-next-jf-4A100/llava_vicuna_simpo_inject_8k_aug_8k_f32_stride_3/checkpoint-100/ simpo-next-f32-stride-3-medium 32 medium True vicuna_v1 2 average no_token 336
