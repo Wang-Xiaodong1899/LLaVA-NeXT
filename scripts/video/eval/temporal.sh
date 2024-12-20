323ccfb1779d772b4fc81c3c774c7cf03be30802
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-20 12:12:16 +0800

    minor

--- scripts/video/eval/temporal.sh
+++ scripts/video/eval/temporal.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# /volsparse3/wxd/ckpt/llava-next-jf-4A100/llava_simpo_8k_debate-hound-ours-sft/checkpoint-500
+
 CKPT=$1
 SAVE_NAME=$2
 FRAMES=$3
@@ -8,7 +10,7 @@ STRIDE=${4:-2}
 #eval_frame: 16 (align with finetuning)
 
 # vicuna
-# bash scripts/video/eval/video_temporal.sh $CKPT vicuna_v1 $FRAMES $STRIDE average no_token True $SAVE_NAME
+bash scripts/video/eval/video_temporal.sh $CKPT vicuna_v1 $FRAMES $STRIDE average no_token True $SAVE_NAME
 
 # ov
 # bash scripts/video/eval/video_temporal.sh $CKPT qwen_1_5 $FRAMES $STRIDE bilinear one_token True $SAVE_NAME
