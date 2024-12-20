998a75562397b726b2ebd07f80d3bd50eb9a010d
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-20 13:31:45 +0800

    minor

--- llava/eval/evaluate/evaluate_benchmark_1_correctness.py
+++ llava/eval/evaluate/evaluate_benchmark_1_correctness.py
@@ -27,7 +27,7 @@ def parse_args():
 
 from openai import OpenAI
 client = OpenAI(
-    base_url="https://api.ai-gaochao.cn/v1/",
+    base_url="https://openrouter.ai/api/v1",
     api_key=os.getenv("API_KEY"),
 )
 # # client = OpenAI(
