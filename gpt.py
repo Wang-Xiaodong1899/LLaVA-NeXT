de507e29752a15a0014b66a5a18e23fa9e8beaca
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-20 18:01:16 +0800

    minor

--- gpt.py
+++ gpt.py
@@ -1,11 +1,12 @@
+import os
 from openai import OpenAI
 client = OpenAI(
-    base_url="https://api.ai-gaochao.cn/v1/",
-    api_key='sk-UYqwq36Z0hmfyaWJ69F675A344D645D79c9dB863Ae870eAd'
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("API_KEY"),
 )
 
 completion = client.chat.completions.create(
-                model="gpt-3.5-turbo-1106",
+                model="openai/gpt-3.5-turbo-0613",
                 messages=[
                     {
                         "role": "system",
