11629306bf789b37f852fb1e4baaf2e57ded6ff0
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-20 18:09:24 +0800

    minor

--- gpt.py
+++ gpt.py
@@ -1,26 +1,49 @@
 import os
-from openai import OpenAI
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("API_KEY"),
+# from openai import OpenAI
+# client = OpenAI(
+#     base_url="https://openrouter.ai/api/v1",
+#     api_key=os.getenv("API_KEY"),
+# )
+
+# completion = client.chat.completions.create(
+#                 model="openai/gpt-3.5-turbo",
+#                 messages=[
+#                     {
+#                         "role": "system",
+#                         "content":
+#                             "You are an intelligent chatbot designed for evaluating the factual accuracy of generative outputs for video-based question-answer pairs. "
+#                     },
+#                     {
+#                         "role": "user",
+#                         "content":
+#                             "hello!"
+#                     }
+#                 ]
+#             )
+
+import requests
+import json
+
+response = requests.post(
+  url="https://openrouter.ai/api/v1/chat/completions",
+  headers={
+    "Authorization": f"Bearer {os.getenv("API_KEY")}",
+  },
+  data=json.dumps({
+    "model": "openai/gpt-3.5-turbo-0613", # Optional
+    "messages": [
+      {
+        "role": "user",
+        "content": "What is the meaning of life?"
+      }
+    ]
+    
+  })
 )
 
-completion = client.chat.completions.create(
-                model="openai/gpt-3.5-turbo",
-                messages=[
-                    {
-                        "role": "system",
-                        "content":
-                            "You are an intelligent chatbot designed for evaluating the factual accuracy of generative outputs for video-based question-answer pairs. "
-                    },
-                    {
-                        "role": "user",
-                        "content":
-                            "hello!"
-                    }
-                ]
-            )
-            # Convert response to a Python dictionary.
-response_message = completion.choices[0].message.content
-print(response_message)
-print(completion.model)
+print(response)
+
+# Convert response to a Python dictionary.
+# response_message = completion.choices[0].message.content
+# print(response_message)
+# print(completion.model)
