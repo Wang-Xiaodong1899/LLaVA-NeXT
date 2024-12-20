37c197feeb272dc28915290b8058f5d23dcd31b0
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-20 19:04:21 +0800

    gpt-3.5-turbo-0125

--- llava/eval/evaluate/evaluate_benchmark_4_temporal.py
+++ llava/eval/evaluate/evaluate_benchmark_4_temporal.py
@@ -61,7 +61,7 @@ def annotate(prediction_set, caption_files, output_dir):
             # Compute the temporal understanding score
             # completion = openai.chat.completions.create(
             completion = client.chat.completions.create(
-                model="openai/gpt-3.5-turbo-0613",
+                model="gpt-3.5-turbo-0125",
                 messages=[
                     {
                         "role": "system",
