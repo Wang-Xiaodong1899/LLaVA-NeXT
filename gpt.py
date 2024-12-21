7a0d9575f9f0dd6f2c2c10a695114e210a022937
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-21 14:51:36 +0800

    minor

--- gpt.py
+++ gpt.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 completion = client.chat.completions.create(
-                model="gpt-3.5-turbo-0125",
+                model="gpt-3.5-turbo-0613",
                 messages=[
                     {
                         "role": "system",
