e7c31dec60df34c287d254558e93f0f771dcfdb8
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-21 14:49:33 +0800

    minor

--- gpt.py
+++ gpt.py
@@ -1,7 +1,7 @@
 import os
 from openai import OpenAI
 client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
+    base_url="https://api.ai-gaochao.cn/v1/",
     api_key=os.getenv("API_KEY"),
 )
 
