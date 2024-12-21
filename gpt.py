267e19c97c5fa1712f5a8524a147420b590578e2
Wang-Xiaodong1899 <1443472206@qq.com>
2024-12-21 14:55:33 +0800

    0125

--- gpt.py
+++ gpt.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 completion = client.chat.completions.create(
-                model="gpt-3.5-turbo-0613",
+                model="gpt-3.5-turbo-0125",
                 messages=[
                     {
                         "role": "system",
