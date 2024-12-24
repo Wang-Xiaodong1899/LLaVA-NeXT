9c6bbbb0d8248de9755b0d78299c3173459a8921
Xiaodong Wang (MAC) <1443472206@qq.com>
2024-12-25 01:16:18 +0800

    minor

--- tsne_set.py
+++ tsne_set.py
@@ -14,10 +14,13 @@ querys = []
 for file in tqdm(files):
     if "qry_tok_hs.pt" in file:
         query_tokens = torch.load(os.path.join(tgt_dir, file))
-        querys.append(query_tokens[0, -1, :])
+        querys.append(query_tokens[: -1, :])
     elif "vid_tok_hs.pt" in file:
         video_tokens = torch.load(os.path.join(tgt_dir, file))
-        videos.append(video_tokens[0, -1, :])
+        videos.append(video_tokens[: -1, :])
+
+videos = torch.cat(videos, dim=0)
+querys = torch.cat(querys, dim=0)
 
 tokens = torch.cat([videos, querys], dim=0)
 
