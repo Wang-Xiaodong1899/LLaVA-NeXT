8204204c401c39e0fc5512eb1f81837f9995ff8b
Xiaodong Wang (MAC) <1443472206@qq.com>
2025-01-07 16:17:30 +0800

    fix mp4 video reading

--- scripts/process/llava-video-178k/self_generate_preference_debate.py
+++ scripts/process/llava-video-178k/self_generate_preference_debate.py
@@ -286,12 +286,10 @@ def run_inference(args):
         # Check if the video exists
         if os.path.exists(video_path):
             if "gpt4v" != args.model_path:
-                video, aug_video = load_video(video_path, args)
+                video = load_video(video_path, args)
                 video = image_processor.preprocess(video, return_tensors="pt")["pixel_values"].half().cuda()
                 video = [video]
                 
-                aug_video = image_processor.preprocess(aug_video, return_tensors="pt")["pixel_values"].half().cuda()
-                aug_video = [aug_video]
             else:
                 video = load_video_base64(video_path)
                 interval = int(len(video) / args.for_get_frames_num)
