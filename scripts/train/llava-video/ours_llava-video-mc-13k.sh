ad4d8328ebcda1b5423157797f6b89ee7de1b573
Wang-Xiaodong1899 <1443472206@qq.com>
2025-01-08 17:52:32 +0800

    minor

--- scripts/train/llava-video/ours_llava-video-mc-13k.sh
+++ scripts/train/llava-video/ours_llava-video-mc-13k.sh
@@ -60,7 +60,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --mm_resampler_type null \
     --group_by_modality_length True \
     --image_aspect_ratio anyres_max_9 \
-    --image_grid_pinpoints "(1x1),...,(6x6)" \
+    --image_grid_pinpoints "(1x1),...,(4x4)" \
     --mm_patch_merge_type spatial_unpad \
     --bf16 True \
     --run_name $WANDB_NAME \
