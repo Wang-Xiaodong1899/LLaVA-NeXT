eadc4422159578ec5672fcce3f93022fd248a9ea
Wang-Xiaodong1899 <1443472206@qq.com>
2025-01-08 20:02:54 +0800

    minor

--- scripts/train/llava-video/ours_llava-video-mc-13k.sh
+++ scripts/train/llava-video/ours_llava-video-mc-13k.sh
@@ -62,7 +62,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --image_aspect_ratio anyres_max_9 \
     --image_grid_pinpoints "(1x1),...,(4x4)" \
     --mm_patch_merge_type spatial_unpad \
-    --bf16 True \
+    --fp16 True \
     --run_name $WANDB_NAME \
     --output_dir $output_dir \
     --num_train_epochs 1 \
@@ -78,7 +78,7 @@ torchrun --nproc_per_node=$n_gpu --master_port=$port \
     --warmup_ratio 0.1 \
     --lr_scheduler_type "linear" \
     --logging_steps 1 \
-    --tf32 True \
+    --tf32 False \
     --model_max_length 4096 \
     --gradient_checkpointing True \
     --dataloader_num_workers 16 \
