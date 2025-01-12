01759a6ebe419bb253ec56eac6a725d1582e7c42
Wang-Xiaodong1899 <1443472206@qq.com>
2025-01-12 19:35:59 +0800

    minor

--- scripts/process/enrich_jsonl.py
+++ scripts/process/enrich_jsonl.py
@@ -3,7 +3,7 @@ import os
 
 matched_data = []
 
-jsonl_file_path = r"C://Users//wangxiaodong//Downloads//ov-72b-f32_0_16000.jsonl"
+jsonl_file_path = "/volsparse3/wxd/data/self-gen/llava-next-debate-video-178k/2_3_m_youtube_v0_1-2_3_m_youtube_oe-0110/llava-next-7b-f16-s2-0_16000.jsonl"
 
 # Read the JSONL file
 with open(jsonl_file_path, 'r', encoding='utf-8') as jsonl_file:
@@ -11,9 +11,9 @@ with open(jsonl_file_path, 'r', encoding='utf-8') as jsonl_file:
         entry = json.loads(line)  # Parse the JSON line into a dictionary
         matched_data.append(entry)  # Add matching entry to the list
 
-matched_data = sorted(matched_data, key=lambda x: x['id'])
+# matched_data = sorted(matched_data, key=lambda x: x['id'])
 
-jsonl_file_path = r"C://Users//wangxiaodong//Downloads//ov-7b-f32_0_16000.jsonl"
+jsonl_file_path = "/volsparse3/wxd/data/self-gen/llava-next-aug-video-178k/2_3_m_youtube_v0_1-2_3_m_youtube_oe-0110/llava-next-7b-f2-s2-0_16000.jsonl"
 
 matched_data_1 = []
 # Read the JSONL file
@@ -22,17 +22,23 @@ with open(jsonl_file_path, 'r', encoding='utf-8') as jsonl_file:
         entry = json.loads(line)  # Parse the JSON line into a dictionary
         matched_data_1.append(entry)  # Add matching entry to the list
 
-matched_data_1 = sorted(matched_data_1, key=lambda x: x['id'])
+# matched_data_1 = sorted(matched_data_1, key=lambda x: x['id'])
 
 new_data = []
-for s1, s2 in zip(matched_data, matched_data_1):
-    if s1["id"] == s2["id"]:
-        # import pdb; pdb.set_trace()
-        s2["chosen"] = s1["rejected"]
-        new_data.append(s2)
+# for s1, s2 in zip(matched_data, matched_data_1):
+#     if s1["id"] == s2["id"]:
+#         # import pdb; pdb.set_trace()
+#         s1["rejected"] = s2["rejected"]
+#         new_data.append(s1)
+for s1 in matched_data:
+    for s2 in matched_data_1:
+        if s1["id"] == s2["id"] and s1["prompt"]==s2["prompt"]:
+            # import pdb; pdb.set_trace()
+            s1["rejected"] = s2["rejected"]
+            new_data.append(s1)
 
 print(len(new_data))
-with open(r"C://Users//wangxiaodong//Downloads//ov-72b-7b_chosen_rejected_0_16000.jsonl", 'w', encoding='utf-8') as output_file:
+with open("/volsparse3/wxd/data/self-gen/llava-next-debate-video-178k/2_3_m_youtube_v0_1-2_3_m_youtube_oe-0110/llava-next-7b-f16-s2-merge-16k.jsonl", 'w', encoding='utf-8') as output_file:
     for item in new_data:
         json.dump(item, output_file)  # Write the JSON entry to the file
         output_file.write('\n')  # Add a newline after each entry
