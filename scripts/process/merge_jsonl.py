44b30003c0a59a6005d3cfae4bd11c0bc538a223
Wang-Xiaodong1899 <1443472206@qq.com>
2025-01-12 19:29:54 +0800

    minor

--- scripts/process/merge_jsonl.py
+++ scripts/process/merge_jsonl.py
@@ -2,8 +2,8 @@ import os
 import json
 
 # Define the input folder path and output file path
-input_folder = r'C:\Users\wangxiaodong\Desktop\checkpoint-500_vicuna_v1_frames_4_stride_2'
-output_file = 'next-dpo-7b-iter2-f4_8000_16000.jsonl'
+input_folder = "/volsparse3/wxd/data/self-gen/llava-next-debate-video-178k/2_3_m_youtube_v0_1-2_3_m_youtube_oe-0110/LLaVA-NeXT-Video-7B_vicuna_v1_frames_1_stride_2/"
+output_file = "/volsparse3/wxd/data/self-gen/llava-next-debate-video-178k/2_3_m_youtube_v0_1-2_3_m_youtube_oe-0110/llava-next-7b-f16-s2-0_16000.jsonl"
 
 # Create the output file
 with open(output_file, 'w', encoding='utf-8') as outfile:
@@ -12,7 +12,7 @@ with open(output_file, 'w', encoding='utf-8') as outfile:
     filenames.sort()
     print(filenames)
     for filename in filenames:
-        if filename.endswith('.jsonl') and filename.startswith('next'):
+        if filename.endswith('.jsonl'):
             with open(os.path.join(input_folder, filename), 'r', encoding='utf-8') as infile:
                 for line in infile:
                     outfile.write(line)
