238d25af299da4f07a75caa4ace8d755e8d99159
Wang-Xiaodong1899 <1443472206@qq.com>
2025-01-12 19:31:51 +0800

    minor

--- scripts/process/merge_jsonl.py
+++ scripts/process/merge_jsonl.py
@@ -2,8 +2,8 @@ import os
 import json
 
 # Define the input folder path and output file path
-input_folder = "/volsparse3/wxd/data/self-gen/llava-next-debate-video-178k/2_3_m_youtube_v0_1-2_3_m_youtube_oe-0110/LLaVA-NeXT-Video-7B_vicuna_v1_frames_1_stride_2/"
-output_file = "/volsparse3/wxd/data/self-gen/llava-next-debate-video-178k/2_3_m_youtube_v0_1-2_3_m_youtube_oe-0110/llava-next-7b-f16-s2-0_16000.jsonl"
+input_folder = "/volsparse3/wxd/data/self-gen/llava-next-aug-video-178k/2_3_m_youtube_v0_1-2_3_m_youtube_oe-0110/LLaVA-NeXT-Video-7B_vicuna_v1_frames_1_stride_2/"
+output_file = "/volsparse3/wxd/data/self-gen/llava-next-aug-video-178k/2_3_m_youtube_v0_1-2_3_m_youtube_oe-0110/llava-next-7b-f2-s2-0_16000.jsonl"
 
 # Create the output file
 with open(output_file, 'w', encoding='utf-8') as outfile:
