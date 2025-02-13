2279ca8f527bafbc564665d1fa1525f124415445
Wang-Xiaodong1899 <1443472206@qq.com>
2025-02-13 16:55:38 +0800

    test mixture logits with reference and policy models

--- trl/trainer/__init__.py
+++ trl/trainer/__init__.py
@@ -39,6 +39,7 @@ if is_diffusers_available():
 from .dpo_trainer import DPOTrainer
 from .cdpo_trainer import CDPOTrainer
 from .fine_dpo_trainer import IPOTrainer
+from .fine_dpo_mix_trainer import MIPOTrainer
 from .iterative_sft_trainer import IterativeSFTTrainer
 from .model_config import ModelConfig
 from .ppo_config import PPOConfig
