8c6fbeb35cd357f639d393a4445f08ccba055b1f
Wang-Xiaodong1899 <1443472206@qq.com>
2025-03-04 17:37:52 +0800

    generate data

--- scripts/process/gen_prefer_next-7b-sample_debate.sh
+++ scripts/process/gen_prefer_next-7b-sample_debate.sh
@@ -35,7 +35,7 @@ fi
 python3 scripts/process/self_generate_preference_debate.py \
     --model-path $CKPT \
     --video_root ${VIDEO_PATH} \
-    --output_dir /volsparse3/wxd/data/self-gen/debate-hound-qa-0102/$SAVE_DIR \
+    --output_dir /volsparse3/wxd/data/self-gen/debate-iter2-hound-qa-0304/$SAVE_DIR \
     --output_name next-7b-f16-s2-${START}_${END} \
     --jsonl-file $JSONLFILE \
     --start $START \
@@ -59,3 +59,6 @@ python3 scripts/process/self_generate_preference_debate.py \
 
 #qa
 # CUDA_VISIBLE_DEVICES=3 bash scripts/process/gen_prefer_next-7b-sample_debate.sh /volsparse3/wxd/models/vicuna/LLaVA-NeXT-Video-7B vicuna_v1 1 2 average no_token True /data/llava_hound/shareVideoGPTV/train_300k_qa_video /data/llava_hound/shareVideoGPTV/chatgpt_qa_900k_ge_70.jsonl 12000 16000 224
+
+# qa
+# CUDA_VISIBLE_DEVICES=0 bash scripts/process/gen_prefer_next-7b-sample_debate.sh /volsparse3/wxd/ckpt/saves/vicuna_llava_simpo_17k_debate-hound-17k-dynalabelsmooth-pilog-0/checkpoint-4000 vicuna_v1 1 2 average no_token True /volsparse3/wxd/data/llava_hound/QA2 /volsparse3/wxd/data/llava_hound/chatgpt_qa_900k_ge_70.jsonl 0 1000 224
