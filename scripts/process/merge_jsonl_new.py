e3ffe289635cb5bd90755600adc1d03decc91935
Wang-Xiaodong1899 <1443472206@qq.com>
2025-03-05 12:21:27 +0800

    minor

--- scripts/process/merge_jsonl_new.py
+++ scripts/process/merge_jsonl_new.py
@@ -15,13 +15,15 @@ def merge_jsonl_files(file_list, output_file):
                     
                     outfile.write(line)
 
-root = "/volsparse3/wxd/data/self-gen/aug-iter2-hound-qa-0304/checkpoint-4000_vicuna_v1_frames_1_stride_2/"
+root = "/volsparse3/wxd/data/self-gen/debate-iter2-hound-qa-0304/checkpoint-4000_vicuna_v1_frames_1_stride_2"
 
-file_list = ['next-7b-f2-s2-0_2000.jsonl', 'next-7b-f2-s2-2000_4000.jsonl', 'next-7b-f2-s2-4000_6000.jsonl', 'next-7b-f2-s2-6000_8000.jsonl']
+file_list = ['next-7b-f16-s2-0_1000.jsonl', 'next-7b-f16-s2-1000_2000.jsonl', 'next-7b-f16-s2-2000_3000.jsonl', 'next-7b-f16-s2-3000_4000.jsonl',
+             'next-7b-f16-s2-4000_5000.jsonl', 'next-7b-f16-s2-5000_6000.jsonl', 'next-7b-f16-s2-6000_7000.jsonl', 'next-7b-f16-s2-7000_8000.jsonl'
+             ]
 
 file_list = [os.path.join(root, f) for f in file_list]
 
-output_file = 'next-7b-f2-s2-8k.jsonl'
+output_file = 'next-7b-f16-s2-8k.jsonl'
 
 output_file = os.path.join(root, output_file)
 
