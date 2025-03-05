8a582d87f001aded01ad92e92d23e7186e0d3fb5
Wang-Xiaodong1899 <1443472206@qq.com>
2025-03-05 16:18:21 +0800

    self-gen caption

--- scripts/process/gen_prefer_next-7b-sample_aug_only.sh
+++ scripts/process/gen_prefer_next-7b-sample_aug_only.sh
@@ -35,7 +35,7 @@ fi
 python3 scripts/process/self_generate_preference_sample_aug_only.py \
     --model-path $CKPT \
     --video_root ${VIDEO_PATH} \
-    --output_dir /volsparse3/wxd/data/self-gen/aug-iter2-hound-qa-0304/$SAVE_DIR \
+    --output_dir /volsparse3/wxd/data/self-gen/aug-iter2-hound-caption-0305/$SAVE_DIR \
     --output_name next-7b-f2-s2-${START}_${END} \
     --jsonl-file $JSONLFILE \
     --start $START \
@@ -59,3 +59,7 @@ python3 scripts/process/self_generate_preference_sample_aug_only.py \
 
 # qa
 # CUDA_VISIBLE_DEVICES=0 bash scripts/process/gen_prefer_next-7b-sample_aug_only.sh /volsparse3/wxd/ckpt/saves/vicuna_llava_simpo_17k_debate-hound-17k-dynalabelsmooth-pilog-0/checkpoint-4000 vicuna_v1 1 2 average no_token True /data/llava_hound/shareVideoGPTV/train_300k/data /volsparse3/wxd/data/llava_hound/chatgpt_qa_900k_ge_70.jsonl 0 2000 224
+
+
+# caption
+# CUDA_VISIBLE_DEVICES=1 bash scripts/process/gen_prefer_next-7b-sample_aug_only.sh /volsparse3/wxd/ckpt/saves/vicuna_llava_simpo_17k_debate-hound-17k-dynalabelsmooth-pilog-0/checkpoint-4000 vicuna_v1 1 2 average no_token True /data/llava_hound/shareVideoGPTV/train_300k/data /volsparse3/wxd/data/llava_hound/ video_240k_caption_15k_ge_16.jsonl 4000 5000 224
